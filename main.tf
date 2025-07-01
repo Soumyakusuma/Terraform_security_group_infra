4136073452a50efdf389cf1a24bfbb3a95ec0784
Soumyakusuma <soumyakusuma2@gmail.com>
2025-07-01 21:28:37 +0530

    nginx install

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ resource "aws_security_group" "roboshop_sg" {
   description = var.sg_description
   vpc_id      = var.vpc_id
 
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = merge(
     var.sg_tags,local.common_tags,{
         Name="${var.project}-${var.environment}-dev"
